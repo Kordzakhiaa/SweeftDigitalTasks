0315195df461ba93db0490be106c049afeddfd23
Kordzakhiaa <giorgi.kordzakhia.2@btu.edu.ge>
2021-12-17 19:07:55 +0400

    Modified: bomberman.py

--- bomberman.py
+++ bomberman.py
@@ -51,4 +51,50 @@ OOO . . . O
 . . . OOOO
 . . . OOOO
 """
+from typing import List
 
+
+def output(r, c, n, grid) -> None:
+    if n % 2 == 0:
+        print('\n'.join('O' * c for _ in range(r)))
+        return
+    m = 2 * max(r, c)
+    if n > m:
+        n = m + (n - m) % 4
+    for i in range(n // 2):
+        grid = go(r, c, n, grid)
+    print('\n'.join(''.join(g) for g in grid))
+
+
+def go(r, c, n, grid) -> List[list]:
+    g = [list('O' * c) for _ in range(r)]
+    for y in range(r):
+        for x, b in enumerate(grid[y]):
+            if b == '.':
+                continue
+            g[y][x] = '.'
+            if y > 0:
+                g[y - 1][x] = '.'
+            if y < r - 1:
+                g[y + 1][x] = '.'
+            if x > 0:
+                g[y][x - 1] = '.'
+            if x < c - 1:
+                g[y][x + 1] = '.'
+    return g
+
+
+def main() -> None:
+    r, c, n = input().split(' ')
+    r, c, n = int(r), int(c), int(n)
+
+    grid = []
+
+    for _ in range(r):
+        grid.append(input())
+    output(r, c, n, grid)
+
+
+if __name__ == '__main__':
+    """ Driver Code """
+    main()
